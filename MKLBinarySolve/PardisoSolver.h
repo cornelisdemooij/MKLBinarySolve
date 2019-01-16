e1a8e9c2b63abd0c6e19c6998cb31f83a02f8125
Cornelis de Mooij <codemo@gmail.com>
2019-01-16 21:47:59 +0100

    Correct solution, variable input size, no error. Ready for phase 2: I/O

--- MKLBinarySolve/PardisoSolver.h
+++ MKLBinarySolve/PardisoSolver.h
@@ -34,176 +34,116 @@ vector<double> solveWithPardiso(vector<int> ARows, vector<int> ACols, vector<dou
         a[i] = AVals[i];
     }
 
-    // PARDISO input parameters:
-    //int nnz = ia[n];      // Number of nonzeros.
-    const int mtype = 11;   // Real unsymmetric matrix.
     double *b = new double[n];  // RHS vector.
     double *x = new double[n];  // Solution vector.
-    int nrhs = 1;           // Number of right hand sides.
-    void *pt[64];           // Internal solver memory pointer pt. 32-bit: int pt[64]; 64-bit: long int pt[64]. Both: void *pt[64].
-    for (int i = 0; i < 64; i++) {
-        pt[i] = 0;
-    }
-    // PARDISO control parameters:
+
+    // PARDISO input parameters:
+    MKL_INT mtype = 11;         // Real unsymmetric matrix.
+    double bs[4], res, res0;
+    MKL_INT nrhs = 1;           // Number of right hand sides.
+    // Internal solver memory pointer pt,
+    // 32-bit: int pt[64]; 64-bit: long int pt[64]
+    // or void *pt[64] should be OK on both architectures:
+    void *pt[64];
+    
+    // Set up Pardiso control parameters:
     MKL_INT iparm[64];
-    //double dparm[64];
-    MKL_INT solver;
     MKL_INT maxfct, mnum, phase, error, msglvl;
-    //int num_procs;          // Number of processors.
-    //char *var;              // Auxiliary variable.
-    //size_t len = 0;
-    double ddum;            // Double dummy variable.
-    int idum;               // Integer dummy variable.
-
-    // Set up PARDISO control parameters:
-    error = 0;
-    solver = 0;     // Use sparse direct solver.
-    pardisoinit(pt, &mtype, &solver);// , iparm, dparm, &error);
-
-    //_dupenv_s(&var, &len, "OMP_NUM_THREADS");   // var = getenv("OMP_NUM_THREADS");
-    //if (var != NULL) {
-    //    sscanf_s(var, "%d", &num_procs);        // sscanf(var, "%d", &num_procs);
-    //}
-    //else {
-    //    printf("Set environment OMP_NUM_THREADS to 1");
-    //    exit(1);
-    //}
-
-    for (int i = 0; i < 64; i++) {
+    MKL_INT i, j;
+    double ddum;        // Double dummy.
+    MKL_INT idum;       // Integer dummy.
+    char *uplo;
+    for ( i = 0; i < 64; i++ ) {
         iparm[i] = 0;
     }
-    //iparm[2] = 1;   // num_procs;
-    iparm[0] = 1;           // No solver default
-    iparm[1] = 2;           // Fill-in reordering from METIS
-    // Numbers of processors, value of OMP_NUM_THREADS
-    iparm[2] = 4;
-    iparm[3] = 0;           // No iterative-direct algorithm
-    iparm[4] = 0;           // No user fill-in reducing permutation
-    iparm[5] = 0;           // Write solution into x
-    iparm[6] = 0;           // Not in use
-    iparm[7] = 2;           // Max numbers of iterative refinement steps
-    iparm[8] = 0;           // Not in use
-    iparm[9] = 13;          // Perturb the pivot elements with 1E-13
-    iparm[10] = 1;          // Use nonsymmetric permutation and scaling MPS
-    iparm[11] = 0;          // Conjugate transposed/transpose solve
-    iparm[12] = 1;          // Maximum weighted matching algorithm is switched-on (default for non-symmetric)
-    iparm[13] = 0;          // Output: Number of perturbed pivots
-    iparm[14] = 0;          // Not in use
-    iparm[15] = 0;          // Not in use
-    iparm[16] = 0;          // Not in use
-    iparm[17] = -1;         // Output: Number of nonzeros in the factor LU
-    iparm[18] = -1;         // Output: Mflops for LU factorization
-    iparm[19] = 0;          // Output: Numbers of CG Iterations
-    //iparm[60 - 1] = 1;      // out of core version
-    //iparm[26] = 0;
-    //iparm[28] = 0;
-    iparm[34] = 1;
-    iparm[59] = 2;  // Use out of core (OOC) version.
+    iparm[0] = 1;       // No solver default.
+    iparm[1] = 2;       // Fill-in reordering from METIS.
+    iparm[2] = 4;       // Numbers of processors, value of OMP_NUM_THREADS.
+    iparm[3] = 0;       // No iterative-direct algorithm.
+    iparm[4] = 0;       // No user fill-in reducing permutation.
+    iparm[5] = 0;       // Write solution into x.
+    iparm[6] = 0;       // Not in use.
+    iparm[7] = 2;       // Max numbers of iterative refinement steps.
+    iparm[8] = 0;       // Not in use.
+    iparm[9] = 13;      // Perturb the pivot elements with 1E-13.
+    iparm[10] = 1;      // Use nonsymmetric permutation and scaling MPS.
+    iparm[11] = 0;      // Conjugate/transpose solve.
+    iparm[12] = 1;      // Maximum weighted matching algorithm is switched-on (default for non-symmetric).
+    iparm[13] = 0;      // Output: Number of perturbed pivots.
+    iparm[14] = 0;      // Not in use.
+    iparm[15] = 0;      // Not in use.
+    iparm[16] = 0;      // Not in use.
+    iparm[17] = -1;     // Output: Number of nonzeros in the factor LU.
+    iparm[18] = -1;     // Output: Mflops for LU factorization.
+    iparm[19] = 0;      // Output: Numbers of CG Iterations.
+    iparm[26] = 1;      // 1 = matrix checker on.
+    iparm[34] = 1;      // Use 0-based indexing.
+    iparm[59] = 2;      // Use out of core (OOC) version.
 
-    maxfct = 1;     // Maximum number of numerical factorizations.
-    mnum = 1;       // Which factorization to use.
-
-    msglvl = 1;     // Print statistical information.
-    error = 0;      // Initialize error flag.
-
-    // Convert matrix from 0-based C-notation to Fortran 1-based notation:
-    cout << "a: " << endl;
-    for (int i = 0; i < nnz; i++) {
-        cout << to_string(a[i]) << " ";
+    maxfct = 1;         // Maximum number of numerical factorizations.
+    mnum = 1;           // Which factorization to use.
+    msglvl = 1;         // Print statistical information in file.
+    error = 0;          // Initialize error flag.
+    
+    // Initialize the internal solver memory pointer. This is only
+    // necessary for the FIRST call of the PARDISO solver:
+    for ( i = 0; i < 64; i++ ) {
+        pt[i] = 0;
     }
-    cout << endl << "ia: " << endl;
-    for (int i = 0; i < n + 1; i++) {
-        //ia[i] += 1;
-        cout << to_string(ia[i]) << " ";
+    
+    // Reordering and Symbolic Factorization. This step also allocates
+    // all memory that is necessary for the factorization:
+    phase = 11;
+    PARDISO (pt, &maxfct, &mnum, &mtype, &phase,
+             &n, a, ia, ja, &idum, &nrhs, 
+             iparm, &msglvl, &ddum, &ddum, &error);
+    if ( error != 0 ) {
+        printf ("\nERROR during symbolic factorization: %d", error);
+        exit (1);
     }
-    cout << endl << "ja: " << endl;
-    for (int i = 0; i < nnz; i++) {
-        //ja[i] += 1;
-        cout << to_string(ja[i]) << " ";
+    printf ("\nReordering completed ... ");
+    printf ("\nNumber of nonzeros in factors = %d", iparm[17]);
+    printf ("\nNumber of factorization MFLOPS = %d", iparm[18]);
+    
+    // Numerical factorization:
+    phase = 22;
+    PARDISO (pt, &maxfct, &mnum, &mtype, &phase,
+             &n, a, ia, ja, &idum, &nrhs, 
+             iparm, &msglvl, &ddum, &ddum, &error);
+    if ( error != 0 ) {
+        printf ("\nERROR during numerical factorization: %d", error);
+        exit (2);
     }
-    cout << endl;
-
-    // Copy contents of b vector to RHS b array:
-    for (int i = 0; i < n; i++) {
+    printf ("\nFactorization completed ... ");
+    
+    // Solution phase:
+    phase = 33;
+    for ( i = 0; i < n; i++ ) {
         b[i] = bVals[i];
     }
 
-    // Reordering and Symbolic Factorization. This step also allocates all memory for the factorization:
-    phase = 11;
-    pardiso(pt, &maxfct, &mnum, &mtype, &phase,
-            &n, a, ia, ja, &idum, &nrhs,
-            iparm, &msglvl, &ddum, &ddum, &error);
+    // Transpose solve is used for systems in CSC format
+    iparm[11] = 2;      // Transpose solve.
+    printf ("\n\nSolving the system in CSC format...\n");
+    PARDISO (pt, &maxfct, &mnum, &mtype, &phase,
+             &n, a, ia, ja, &idum, &nrhs, 
+             iparm, &msglvl, b, x, &error);
     if (error != 0) {
-        printf("\nERROR during symbolic factorization: %d", error);
-        exit(1);
+        printf ("\nERROR during solution: %d", error);
+        exit (3);
     }
-    printf("\nReordering completed ...\n");
-    printf("\nNumber of nonzeros in factors  = %d", iparm[17]);
-    printf("\nNumber of factorization MFLOPS = %d", iparm[18]);
 
-    // Numerical factorization:
-    phase = 22;
-    pardiso(pt, &maxfct, &mnum, &mtype, &phase,
-            &n, a, ia, ja, &idum, &nrhs,
-            iparm, &msglvl, &ddum, &ddum, &error);
-    if (error != 0) {
-    printf("\nERROR during numerical factorization: %d", error);
-    exit(2);
+    printf ("\nThe solution of the system is: ");
+    for (j = 0; j < n; j++) {
+        printf ("\n x [%d] = % f", j, x[j]);
     }
-    printf("\nFactorization completed ...\n");
-
-    // Back substitution and iterative refinement:
-    phase = 33;
-    iparm[7] = 1;       // Max numbers of iterative refinement steps.
-    pardiso(pt, &maxfct, &mnum, &mtype, &phase,
-            &n, a, ia, ja, &idum, &nrhs,
-            iparm, &msglvl, b, x, &error);
-    if (error != 0) {
-        printf("\nERROR during solution: %d", error);
-        exit(3);
-    }
-    printf("\nSolve completed ...\n");
-    printf("\nThe solution of the system is: ");
-    for (int i = 0; i < n; i++) {
-        printf("\n x [%d] = %f", i, x[i]);
-    }
-    printf("\n");
-
-    // Back substitution with transposed matrix A^t x = b
-    phase = 33;
-    iparm[7] = 1;       // Max numbers of iterative refinement steps.
-    iparm[11] = 1;      // Solving with transpose matrix.
-    // Copy contents of b vector to RHS b array:
-    for (int i = 0; i < n; i++) {
-    b[i] = bVals[i];
-    }
-    pardiso(pt, &maxfct, &mnum, &mtype, &phase,
-            &n, a, ia, ja, &idum, &nrhs,
-            iparm, &msglvl, b, x, &error);
-    if (error != 0) {
-        printf("\nERROR during solution: %d", error);
-        exit(3);
-    }
-    printf("\nSolve completed ...\n");
-    printf("\nThe solution of the system is: ");
-    for (int i = 0; i < n; i++) {
-        printf("\n x [%d] = %f", i, x[i]);
-    }
-    printf("\n");
-
-    // Convert matrix back to 0-based C-notation:
-    for (int i = 0; i < n + 1; i++) {
-        //ia[i] -= 1;
-    }
-    for (int i = 0; i < nnz; i++) {
-        //ja[i] -= 1;
-    }
+    printf ("\n");
 
     // Termination and release of memory:
-    phase = -1;     // Release internal memory.
-    pardiso(pt, &maxfct, &mnum, &mtype, &phase,
-            &n, &ddum, ia, ja, &idum, &nrhs,
-            iparm, &msglvl, &ddum, &ddum, &error);
+    phase = -1;         // Release internal memory.
+    PARDISO (pt, &maxfct, &mnum, &mtype, &phase,
+             &n, &ddum, ia, ja, &idum, &nrhs,
+             iparm, &msglvl, &ddum, &ddum, &error);
 
     // Convert x to vector and return it:
     vector<double> result(n);
