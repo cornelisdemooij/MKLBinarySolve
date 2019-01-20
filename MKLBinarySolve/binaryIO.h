baa747f37016006b94956b650a01f56a9851bf05
Cornelis de Mooij <codemo@gmail.com>
2019-01-21 00:27:50 +0100

    Implemented ability to read A matrix and b vector from binary file and solve for x. Exporting x to binary file is next.

--- MKLBinarySolve/binaryIO.h
+++ MKLBinarySolve/binaryIO.h
@@ -7,18 +7,58 @@
 
 using namespace std;
 
-vector<double> getVector(string filename, int length) {
-    // Read vector from result file:
+class COOMatrix {
+public:
+    vector<int> rows;
+    vector<int> cols;
+    vector<double> vals;
+};
+
+COOMatrix getCOOMatrix(string filename) {
+    // Read matrix from file. Matrix must be in COO format, sorted in row-first order.
     ifstream file;
     file.open(filename, ios::in | ios::binary);
-    vector<double> v = vector<double>(0);
-    int i = 0;
+    
+    int nnz;
+    file.read((char*)&(nnz), sizeof(int));
+
+    COOMatrix matrix = COOMatrix();
+    matrix.rows = vector<int>(nnz);
+    matrix.cols = vector<int>(nnz);
+    matrix.vals = vector<double>(nnz);
+    
+    int row;
+    int col;
     double val;
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < nnz; i++) {
+        file.read((char*)&(row), sizeof(int));
+        file.read((char*)&(col), sizeof(int));
         file.read((char*)&(val), sizeof(double));
-        v.push_back(val);
+
+        matrix.rows[i] = row;
+        matrix.cols[i] = col;
+        matrix.vals[i] = val;
     }
+
+    file.close();
+    return matrix;
+}
+vector<double> getVector(string filename) {
+    // Read vector from file.
+    ifstream file;
+    file.open(filename, ios::in | ios::binary);
+
+    int n;
+    file.read((char*)&(n), sizeof(int));
+
+    vector<double> v = vector<double>(n);
+
+    double val;
+    for (int i = 0; i < n; i++) {
+        file.read((char*)&(val), sizeof(double));
+        v[i] = val;
+    }
+
     file.close();
     return v;
 }
-
